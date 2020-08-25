0233859b5ba4d1c1ab092d33b9968d89e8ad7842
Sisi Freeley <liusi33@gmail.com>
2020-08-25 10:31:22 -0500

    phase 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMessage();
+        }
+
+        static void DisplayMessage()
+        {
+            Console.Write("Guess a number. ");
+            string response = Console.ReadLine();
+            Console.WriteLine(response);
+        }
+    }
+}
